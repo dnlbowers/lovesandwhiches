21a8744ee44c2c10f6ee95cecd4f865c9f27ec47
dnlbowers <dnlbowers@gmail.com>
2021-11-03 15:50:47 +0000

    Add user input to google sheets when data inputted is valid

--- run.py
+++ run.py
@@ -56,4 +56,16 @@ def validate_data(values):
     return True
 
 
+def update_sale_worksheet(data):
+    """
+    Update sales worksheet with new row from the input provided by the user
+    """
+    print("Updating sales worksheet.....\n")
+    sales_worksheet = SHEET.worksheet('sales')
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updated successfully\n")
+
+
 data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sale_worksheet(sales_data)
