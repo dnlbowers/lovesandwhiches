beaaa9828e8301a533e5fa6c5bdb3510cabdf55b
dnlbowers <dnlbowers@gmail.com>
2021-11-03 21:26:00 +0000

    wrap all global functions in a fucntion, and retrieve latest numbers from the stock list

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -66,6 +67,31 @@ def update_sale_worksheet(data):
     print("Sales worksheet updated successfully\n")
 
 
-data = get_sales_data()
-sales_data = [int(num) for num in data]
-update_sale_worksheet(sales_data)
+def calculate_surplus_data(sale_row):
+    """
+    Compare sales with stock and calculate the surplus for each item type.
+    The surplus is defined as the sales figure subtracted from the stock:
+    - Positive surplus indicates waste
+    - Negative surplus indicates extra made when stock was sold out.
+    """
+    print("Calculating surplus data.....\n")
+    stock = SHEET.worksheet('stock').get_all_values()
+    # a special way to print a list of lists....
+    # # pprint(stock)
+    # ^ remember to import pprint at the top of the file ^
+    stock_row = stock[-1]
+    print(stock_row)
+
+
+def main():
+    """
+    Run all program functions
+    """
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sale_worksheet(sales_data)
+    calculate_surplus_data(sales_data)
+
+
+print("welcome to the Love sandwhiches Data automation")
+main()
